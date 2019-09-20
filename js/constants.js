5f7de31b4b7ca6327494b54892bae26f4df29515
Robinson Luiz <facavocerbn@outlook.com>
2019-09-19 22:46:06 -0300

    Constants

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,4 @@
-let rotaDaApi = 'http://localhost'
+let rotaDaApi = 'https://spacetea.herokuapp.com'
 let variaveisDeAmbiente = {
     alunoLogin: `${rotaDaApi}/api/aluno/auth`,
     jogosPorAluno: `${rotaDaApi}/api/aluno/:id/jogos`
