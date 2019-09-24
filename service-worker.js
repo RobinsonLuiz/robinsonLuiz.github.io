4ebdee3c644ad3bcccd47b0a78828779c7996ad8
Robinson Luiz <facavocerbn@outlook.com>
2019-09-24 01:13:08 -0300

    Update service worker

--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,15 @@ this.addEventListener('activate', function (event) {
     console.log('[Service Worker] Activating Service Worker ....', event);
 });
 
-// this.addEventListener('fetch', function (event) {
-//     event.respondWith(
-//         caches.match(event.request).then(function (response) {
-//             if (response) {
-//                 return response;
-//             } else {
-//                 return fetch(event.request).then(function (res) {
-//                     return caches.open('dynamic').then(function (cache) {
-//                         cache.put(event.request.url, res.clone());
-//                         return res;
-//                     });
-//                 });
-//             }
-//         })
-//     );
-// });
+this.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return fetch(event.request).then(function (res) {
+                return caches.open('dynamic').then(function (cache) {
+                    cache.put(event.request.url, res.clone());
+                    return res;
+                });
+            });
+        })
+    );
+});
