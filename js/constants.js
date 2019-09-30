58ae45a2e539e62289e52e3d4a32d9b5cdc880b3
Robinson Luiz <facavocerbn@outlook.com>
2019-09-29 21:11:17 -0300

    Update contatns

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,4 @@
-let rotaDaApi = 'http://localhost' //'https://spacetea.herokuapp.com'
+let rotaDaApi = 'https://spacetea.herokuapp.com' //'http://localhost'
 let variaveisDeAmbiente = {
     alunoLogin: `${rotaDaApi}/api/aluno/auth`,
     jogosPorAluno: `${rotaDaApi}/api/aluno/:id/jogos`
