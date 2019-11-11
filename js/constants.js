de26bbb24ce336c0ff62096644fc9121a75c87dc
Robinson Luiz <facavocerbn@outlook.com>
2019-11-11 20:20:19 -0300

    Update Constants

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,4 @@
-let rotaDaApi = 'https://playtea.riogrande.ifrs.edu.br:8888' //'https://playtea.riogrande.ifrs.edu.br:8888' //'https://spacetea.herokuapp.com'
+let rotaDaApi = 'http://playtea.riogrande.ifrs.edu.br:8888' //'https://playtea.riogrande.ifrs.edu.br:8888' //'https://spacetea.herokuapp.com'
 let variaveisDeAmbiente = {
     alunoLogin: `${rotaDaApi}/api/aluno/auth`,
     jogosPorAluno: `${rotaDaApi}/api/aluno/:id/jogos`
